352db564073ac89eacde4de2463c40aed96f46f3
vimal <vimal.sasikumar@colanonline.com>
2024-06-04 19:52:40 +0530

    event history api changes

--- app/Http/Controllers/API/ArtistController.php
+++ app/Http/Controllers/API/ArtistController.php
@@ -844,7 +844,6 @@ class ArtistController extends Controller
             $totData['event_plan_type'] = (int) $eventHistory->event_plan_type;
             $eventimage = explode(',', $eventHistory->event_image);
             $totData['event_image'] = asset('/eventimages/' . $eventimage[0]);
-            if(!$eventHistory->eventJoinedByFans){
             $userdetail=[];
             foreach ($eventHistory->eventJoinedByFans as $i=>$value) {
                 $userdetail['user'][$i]['id'] = $value->id;
@@ -858,13 +857,10 @@ class ArtistController extends Controller
                 $userdetail['user'][$i]['created_at'] = $value->created_at;
                 $userdetail['user'][$i]['updated_at'] = $value->updated_at;
             }
-        }else{
-            $userdetail=null;
-        }
             $data = [
                 "event_detail" => $totData,
                 "ratings_total" => $ratings,
-                "user_detail" => $userdetail,
+                "user_detail" => (empty($userdetail)) ? null : $userdetail ,
                 "fans_booked" => optional($eventHistory->eventBookingList)->count(),
                 "fans_participation" => optional($eventHistory->eventJoinedByFans)->count(),
                 "fans_action_average" => $actionAverage,
